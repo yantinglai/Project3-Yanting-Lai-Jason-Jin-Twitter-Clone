ce43f08036e92621359248d205df63e14084750d
jasonkim0110 <jasonkim900110@gmail.com>
2023-04-05 18:46:18 -0700

    project 3

---
+++ routes/registerRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+router.get('/', (req, res, next) => {
+  res.status(200).render('register');
+});
+
+router.post('/', (req, res, next) => {
+  var firstName = req.body.firstName.trim();
+  var lastName = req.body.lastName.trim();
+  var username = req.body.userName.trim();
+  var email = req.body.email.trim();
+  var password = req.body.password;
+
+  var payload = req.body;
+
+  if (firstName && lastName && username && email && password) {
+  } else {
+    payload.errorMessage = 'Make sure each field has a valid value.';
+    res.status(200).render('Register', payload);
+  }
+});
+
+module.exports = router;
