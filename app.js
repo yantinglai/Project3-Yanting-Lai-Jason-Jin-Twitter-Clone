6b10da03a294547fd8869f8e2ec628da0b1852ab
Yanting Lai <laiyanting.neu@gmail.com>
2023-04-08 15:28:47 -0700

    Connected Frontend with backend, login and registration logic

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3003;
 const middleware = require('./middleware');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('./database');
+const session = require('express-session');
 
 const server = app.listen(port, () =>
   console.log('Server listening on port ' + port)
@@ -14,6 +16,13 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'bbq chips',
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
 //Routes
 const loginRoute = require('./routes/loginRoutes');
@@ -25,6 +34,7 @@ app.use('/register', registerRoute);
 app.get('/', middleware.requiredLogin, (req, res, next) => {
   var payload = {
     pageTitle: 'Home',
+    userLoggedIn: req.session.user,
   };
   res.status(200).render('Home', payload);
 });
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const User = require('../schemas/UserSchema');
+const bcrypt = require('bcrypt');
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -12,19 +14,49 @@ router.get('/', (req, res, next) => {
   res.status(200).render('register');
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   var firstName = req.body.firstName.trim();
   var lastName = req.body.lastName.trim();
-  var username = req.body.userName.trim();
+  var username = req.body.username.trim();
   var email = req.body.email.trim();
   var password = req.body.password;
 
   var payload = req.body;
 
   if (firstName && lastName && username && email && password) {
+    var user = await User.findOne({
+      $or: [{ username: username }, { email: email }], // MongoDB operator
+    }).catch((error) => {
+      console.log(error);
+      payload.errorMessage = 'Something went wrong.';
+      res.status(200).render('register', payload);
+    });
+    if (user == null) {
+      // if no user found - insert user into the database
+
+      var data = req.body;
+      data.password = await bcrypt.hash(password, 10); // hashing calculation
+      User.create(data).then((user) => {
+        req.session.user = user;
+        return res.redirect('/'); // return user to the homepage
+      });
+    } else {
+      // if we found user
+
+      if (email == user.email) {
+        // if email matches the user already has
+
+        payload.errorMessage = 'Email already in use.';
+      } else {
+        // if username already exist in the database
+
+        payload.errorMessage = 'Username already in use.';
+      }
+      res.status(200).render('register', payload);
+    }
   } else {
     payload.errorMessage = 'Make sure each field has a valid value.';
-    res.status(200).render('Register', payload);
+    res.status(200).render('register', payload);
   }
 });
 
